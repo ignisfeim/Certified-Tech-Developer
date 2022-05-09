eaffaba38653ab69c88fb7219f2f3164c17b15e0
ignisfeim <ignisfeim@gmail.com>
2022-05-09 20:54:11 -0300

    novo Repositorio Javascript

---
+++ Programação Imperativa/JavaScript/Aula4/aula4.js
@@ -0,0 +1,12 @@
+// console.log('Olá Mundo');
+
+let nome = "Diego Junio";
+const cpf = "700.682.896-18";
+let idade = 22;
+let usuarioAtivo = true;
+
+console.log("Dados usuario");
+console.log( nome );
+console.log( cpf );
+console.log ( idade );
+console.log( usuarioAtivo );
---
+++ Programação Imperativa/JavaScript/Aula4/exercicio4.js
@@ -0,0 +1,16 @@
+const nome = "José";
+let idade = 27;
+let peso = 83.5;
+let altura = 1.70;
+let plano = ("Ouro");
+let mult = ( altura * altura )
+let IMC = ( peso / mult)
+
+
+
+console.log(nome);
+console.log(idade);
+console.log(peso);
+console.log(altura);
+console.log(plano);
+console.log(IMC)
